609aff4a7098578fb28bc1f73db26308b31b6ac2
Arif <faizin.arief@gmail.com>
2023-08-08 11:22:02 +0700

    add camera permission

--- app/src/main/java/com/dicoding/tfliteimageclassification/MainActivity.kt
+++ app/src/main/java/com/dicoding/tfliteimageclassification/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.dicoding.tfliteimageclassification
 
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
 import androidx.camera.core.Preview
@@ -13,10 +15,23 @@ import com.dicoding.tfliteimageclassification.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            val message = if (isGranted) {
+                "Camera permission granted"
+            } else {
+                "Camera permission rejected"
+            }
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        requestPermissionLauncher.launch(Manifest.permission.CAMERA)
     }
 }
