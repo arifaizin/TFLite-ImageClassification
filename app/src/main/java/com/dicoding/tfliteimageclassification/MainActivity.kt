95f785f66e0004368ac7d31d164c196301fbc0dd
Arif <faizin.arief@gmail.com>
2023-08-08 10:42:58 +0700

    implement viewbinding

--- app/src/main/java/com/dicoding/tfliteimageclassification/MainActivity.kt
+++ app/src/main/java/com/dicoding/tfliteimageclassification/MainActivity.kt
@@ -8,10 +8,15 @@ import androidx.camera.core.ImageCapture
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
+import com.dicoding.tfliteimageclassification.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
