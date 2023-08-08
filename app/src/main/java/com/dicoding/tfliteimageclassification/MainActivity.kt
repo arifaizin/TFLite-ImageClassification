35e4631559df8b8915b44cab61b2c38e8659f008
Ahmad Arif Faizin <faizin.arief@gmail.com>
2023-08-08 10:39:18 +0700

    Initial commit

---
+++ app/src/main/java/com/dicoding/tfliteimageclassification/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.dicoding.tfliteimageclassification
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
